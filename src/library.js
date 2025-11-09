b1ca398112656fe941cb1407eb0ba72342c3737c
LewdLeah <daisy.anais.197@gmail.com>
2025-11-08 20:15:51 -0500

    Update library.js

--- src/library.js
+++ src/library.js
@@ -1,7 +1,7 @@
 // Your "Library" tab should look like this
 
 /*
-Discovery Journal v0.0.1
+Discovery Journal v0.0.3
 Made by LewdLeah on October 28, 2025
 */
 if ((globalThis.info?.maxChars !== undefined) || !globalThis.state?.DiscoveryJournal?.scores) {
